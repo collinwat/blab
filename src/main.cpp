f337adf26d7fa93a4e80b029100498fd94ebc624
Collin Watson <collinwat@gmail.com>
2020-11-26 23:36:21 -0800

    Moved example to new directory and building only for main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <heltec.h>
 
+const bool DISPLAY_ENABLED = true;
+const bool LORA_ENABLED = true;
+const int64_t LORA_FREQUENCY = 470E6;
+const bool LORA_POWER_BOOST = true;
+const bool SERIAL_ENABLED = true;
+
 void setup() {
-  Heltec.begin(true /*DisplayEnable Enable*/, true /*LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, 470E6 /**/);
+  Heltec.begin(
+      DISPLAY_ENABLED,
+      LORA_ENABLED,
+      SERIAL_ENABLED,
+      LORA_POWER_BOOST,
+      LORA_FREQUENCY);
 }
 
 void loop() {
